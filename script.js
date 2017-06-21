01e0c2b88bfdec3840982b4f6c415bd23935aec8
Emily Shapiro <emily@scripted.org>
2017-06-21 15:41:07 +0000

    commit message

--- script.js
+++ script.js
@@ -2,5 +2,24 @@
 // JavaScript File
 $( document ).ready(function() {
 
+$(".submit").click(function() {
+    var answer = $("input").val();
+    console.log("button click");
+    if (answer==="yes") {
+    alert("Of course you are!");
+    console.log("yes");
+    }
+    else if (answer==="no"){
+    alert ("Try again, there is only one other option");
+    }
+      else {
+    alert ("Come on friend, it is a yes or no question");
+    }
+});
+
+$(".season").click(function(){
+    $(".body").css("background-image",'url("http://dreamatico.com/data_images/summer/summer-5.jpg")');
+});
+
 
 });
